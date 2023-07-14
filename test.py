562d6c214aa6067fc8446024f1dcb8a8fa827202
erportomar <erasmusmartyna@gmail.com>
2023-07-14 16:03:42 +0200

    sjidsadhisahdashu

---
+++ test.py
@@ -0,0 +1,9 @@
+light = input("what color is the traffic light? (red, green, yellow)")
+if light == "red":
+    print("wait!!!")
+elif light == "yellow":
+    print("get ready!!!")
+elif light == "green":
+    print("gooo!!!!!!!!!!!")
+else:
+    print("wrong answer")
